248bc8aefc5bc25764679eb3abe06fa5de181dbc
Erk Dungan <erik@bigfolio.com>
2009-03-06 16:18:29 -0800

    redirect to fresh tickets after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
       self.current_user = user
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
-      redirect_back_or_default('/')
+      redirect_back_or_default(fresh_tickets_url)
       flash[:notice] = "Logged in successfully"
     else
       note_failed_signin
