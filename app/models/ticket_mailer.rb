1e6c4b658680e8912bc114c44dae316252dc575b
Erk Dungan <erik@bigfolio.com>
2009-11-02 08:38:08 -0800

    trying to update from name in emails

--- app/models/ticket_mailer.rb
+++ app/models/ticket_mailer.rb
@@ -3,7 +3,7 @@ class TicketMailer < ActionMailer::Base
   def ticket_alert(user, ticket)
     # Sent to staff when ticket is submitted
     recipients    user.email
-    from          "help@bigfolio.com"
+    from          "BIG Folio Help <help@bigfolio.com>"
     reply_to      "help@bigfolio.com"
     subject       "New support request (#{ticket.key}) from #{ticket.name}"
     body          :ticket => ticket
@@ -14,7 +14,7 @@ class TicketMailer < ActionMailer::Base
   def ticket_update(user, message, ticket)
     # Sent to staff when the customer adds info
     recipients    user.email
-    from          "help@bigfolio.com"
+    from          "BIG Folio Help <help@bigfolio.com>"
     reply_to      "help@bigfolio.com"
     subject       "New reply from #{ticket.name} for support request [#{ticket.key}]"
     body[:message] = message
@@ -26,7 +26,7 @@ class TicketMailer < ActionMailer::Base
   def ticket_update_with_attachment(user, message, ticket)
     # Sent to staff when the customer adds info
     recipients    user.email
-    from          "help@bigfolio.com"
+    from          "BIG Folio Help <help@bigfolio.com>"
     reply_to      "help@bigfolio.com"
     subject       "New reply from #{ticket.name} for support request [#{ticket.key}]"
     body[:message] = message
@@ -41,7 +41,7 @@ class TicketMailer < ActionMailer::Base
   def ticket_reply(message, ticket)
     # Sent to customer when staff submits a reply
     recipients    ticket.email
-    from          "help@bigfolio.com"
+    from          "BIG Folio Help <help@bigfolio.com>"
     reply_to      "help@bigfolio.com"
     subject       "Update on your support request [#{ticket.key}]"
     body[:message] = message
@@ -53,7 +53,7 @@ class TicketMailer < ActionMailer::Base
   def ticket_reply_with_attachment(message, ticket) 
     # Sent to customer (with attachment) when staff submits a reply
     recipients    ticket.email
-    from          "help@bigfolio.com"
+    from          "BIG Folio Help <help@bigfolio.com>"
     reply_to      "help@bigfolio.com"
     subject       "Update on your support request [#{ticket.key}]"
     body[:message] = message
@@ -68,7 +68,7 @@ class TicketMailer < ActionMailer::Base
   def ticket_confirmation(ticket) 
     # Sent to customer to confirm their submission
     recipients    ticket.email
-    from          "help@bigfolio.com"
+    from          "BIG Folio Help <help@bigfolio.com>"
     reply_to      "help@bigfolio.com"
     subject       "Your support request [#{ticket.key}] has been recieved"
     body          :ticket => ticket
